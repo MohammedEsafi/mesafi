b0c98896e3cde3e561a93d2b39f72c8d868817e6
Mohammed Esafi <hello@mesafi.com>
2021-03-08 12:09:38 +0100

    import / resolver aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,11 +6,9 @@ exports.onCreateWebpackConfig = ({ actions }) => {
 			alias: {
 				'@components': path.join(__dirname, 'src', 'components'),
 				'@styles': path.join(__dirname, 'src', 'styles'),
-				// '@widgets': path.join(__dirname, 'src', 'widgets'),
-				'@fonts': path.join(__dirname, 'src', 'fonts'),
-				// '@config': path.join(__dirname, 'src', 'config'),
 				'@utils': path.join(__dirname, 'src', 'utils'),
-				'@hooks': path.join(__dirname, 'src', 'hooks')
+				'@hooks': path.join(__dirname, 'src', 'hooks'),
+				'@config': path.join(__dirname, 'src', 'config')
 			}
 		}
 	});
