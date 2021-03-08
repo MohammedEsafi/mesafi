75e425e763bdf2e388bec99a6a70a12f9c82099e
Mohammed Esafi <hello@mesafi.com>
2021-03-08 12:11:40 +0100

    Implementation of useMode() hook

---
+++ src/hooks/useMode.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { defaultTheme } from '@config';
+
+const useMode = () => {
+	const [mode, setMode] = useState(defaultTheme);
+
+	useEffect(() => {
+		const savedMode = window?.localStorage.getItem('mode');
+
+		if (!savedMode) {
+			if (window?.matchMedia?.('(prefers-color-scheme: light)').matches) setMode('light');
+		} else {
+			setMode(savedMode);
+		}
+	}, []);
+
+	const toggleMode = () => {
+		if (mode === 'light') {
+			setMode('dark');
+			window.localStorage.setItem('mode', 'dark');
+		} else {
+			setMode('light');
+			window.localStorage.setItem('mode', 'light');
+		}
+	};
+
+	return [mode, toggleMode];
+};
+
+export default useMode;
