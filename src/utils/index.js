b2b7b241ff4be3bacffc307af467402136cc6a9f
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-09 17:35:10 +0100

    feat: add ScrollReveal and isSSR utilities

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,14 @@
-export const clamping = (minWidth, maxWidth, minValue, maxValue) => {
+import ScrollReveal from 'scrollreveal';
+
+const clamping = (minWidth, maxWidth, minValue, maxValue) => {
 	const slope = (maxValue - minValue) / (maxWidth - minWidth);
 	const intersection = -minWidth * slope + minValue;
 
 	return `clamp(${minValue}rem, ${intersection}rem + ${slope * 100}vw, ${maxValue}rem)`;
 };
+
+const isSSR = typeof window === 'undefined';
+
+const sr = isSSR ? null : ScrollReveal();
+
+export { clamping, isSSR, sr };
