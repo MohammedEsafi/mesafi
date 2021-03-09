7448d9bc1347fcd17433d45d9fe0a06e7d4a998d
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-09 18:57:26 +0100

    fix: resolve window is not defined in SSR context

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,21 @@
 const path = require('path');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+// https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+	// https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
+	if (stage === 'build-html' || stage === 'develop-html') {
+		actions.setWebpackConfig({
+			module: {
+				rules: [
+					{
+						test: /scrollreveal/,
+						use: loaders.null()
+					}
+				]
+			}
+		});
+	}
+
 	actions.setWebpackConfig({
 		resolve: {
 			alias: {
