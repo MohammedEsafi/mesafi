65f2803d97a494bfff1153672a265a4964ce66f4
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-09 17:35:33 +0100

    feat: scaffold navigation bar component

---
+++ src/components/menu.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Flex } from '@styles';
+import { navLinks } from '@config';
+
+const UL = styled(Flex)`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+
+	& > li:not(:last-child) {
+		margin-right: 30px;
+	}
+
+	& > li > a {
+		color: ${({ theme }) => theme.onBackground};
+	}
+`;
+
+const Menu = () => (
+	<Flex as='nav'>
+		<UL as='ul'>
+			{navLinks?.map(({ name, url }, index) => (
+				<li key={index}>
+					<a href={url}>{name}</a>
+				</li>
+			))}
+		</UL>
+	</Flex>
+);
+
+export default Menu;
