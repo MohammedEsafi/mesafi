0a8b37e5f8e3e98f144297222325c72e76f517ed
Mohammed Esafi <hello@mesafi.com>
2021-03-09 12:59:08 +0100

    add navigation bar links & edit main padding sizes

--- src/config.js
+++ src/config.js
@@ -1,14 +1,31 @@
 module.exports = {
 	defaultTheme: 'dark',
 
+	// media query breakpoints in `rem` unit
 	breakpoints: {
 		desktop: 1200 / 16,
 		tablet: 768 / 16,
 		phone: 480 / 16
 	},
 
+	// inline start and end padding of every main section in `rem` unit
 	padding: {
-		max: 8,
-		min: 1.5
-	}
+		min: 1.25,
+		max: 3.25
+	},
+
+	navLinks: [
+		{
+			name: 'About',
+			url: '/#about'
+		},
+		{
+			name: 'Work',
+			url: '/#projects'
+		},
+		{
+			name: 'Contact',
+			url: '/#contact'
+		}
+	]
 };
