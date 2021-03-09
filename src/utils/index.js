a509806187c4955cccdc7484e7a54379ef816967
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-09 18:56:36 +0100

    refactor: separate ScrollReveal instance

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,6 @@
-import ScrollReveal from 'scrollreveal';
-
-const clamping = (minWidth, maxWidth, minValue, maxValue) => {
+export const clamping = (minWidth, maxWidth, minValue, maxValue) => {
 	const slope = (maxValue - minValue) / (maxWidth - minWidth);
 	const intersection = -minWidth * slope + minValue;
 
 	return `clamp(${minValue}rem, ${intersection}rem + ${slope * 100}vw, ${maxValue}rem)`;
 };
-
-const isSSR = typeof window === 'undefined';
-
-const sr = isSSR ? null : ScrollReveal();
-
-export { clamping, isSSR, sr };
