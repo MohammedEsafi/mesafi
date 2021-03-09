30040153b4d7f340feab00659fd7a4d32cc14ca9
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-09 07:33:59 +0100

    feat: scaffold header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,25 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
 
-const Header = ({ siteTitle }) => (
-	<header
-		style={{
-			background: `rebeccapurple`,
-			marginBottom: `1.45rem`
-		}}
-	>
-		<div
-			style={{
-				margin: `0 auto`,
-				maxWidth: 960,
-				padding: `1.45rem 1.0875rem`
-			}}
-		>
-			<h1 style={{ margin: 0 }}>
-				<Link
-					to='/'
-					style={{
-						color: `white`,
-						textDecoration: `none`
-					}}
-				>
-					{siteTitle}
-				</Link>
-			</h1>
-		</div>
-	</header>
+const Wrapper = styled.header`
+	width: 100%;
+	max-width: var(--max-width);
+	padding: var(--padding);
+	background-color: red;
+`;
+
+const Header = () => (
+	<Wrapper>
+		<p>Header</p>
+	</Wrapper>
 );
 
-Header.propTypes = {
-	siteTitle: PropTypes.string
-};
+// Header.propTypes = {
+// 	siteTitle: PropTypes.string
+// };
 
-Header.defaultProps = {
-	siteTitle: ``
-};
+// Header.defaultProps = {
+// 	siteTitle: ``
+// };
 
 export default Header;
