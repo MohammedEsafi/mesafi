9e86842a746ed5f7f6bfd5fe9bfaa34c9e6a015c
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-11 12:43:13 +0100

    feat: animate menu text items

---
+++ src/components/Header/menu/title.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Finger = styled.span`
+	display: inline-block;
+`;
+
+const Title = ({ text }) => {
+	const Animate = (event) => {
+		const target = event.currentTarget;
+
+		target.animate(
+			[
+				{ transform: 'scale(1, 1)' },
+				{ transform: 'scale(1.25, .75)' },
+				{ transform: 'scale(.75, 1.25)' },
+				{ transform: 'scale(1.15, .85)' },
+				{ transform: 'scale(.95, 1.05)' },
+				{ transform: 'scale(1.05, .95)' },
+				{ transform: 'scale(1, 1)' }
+			],
+			{
+				duration: 1000,
+				easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+				fill: 'forwards'
+			}
+		);
+	};
+
+	return (
+		<>
+			{text &&
+				[...text].map((finger, index) => {
+					if (finger.match(/\s/)) return finger;
+					return (
+						<Finger key={index} aria-hidden='true' onMouseOver={Animate} onFocus={Animate}>
+							{finger}
+						</Finger>
+					);
+				})}
+		</>
+	);
+};
+
+Title.propTypes = {
+	text: PropTypes.string.isRequired
+};
+
+export default Title;
