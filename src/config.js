f54a2c948e35e714d33f551beee47393b567c0e9
Mohammed Esafi <hello@mesafi.com>
2021-03-15 10:24:48 +0100

    scrollreveal config

--- src/config.js
+++ src/config.js
@@ -40,5 +40,21 @@ module.exports = {
 			name: 'Contact',
 			url: '/#contact'
 		}
-	]
+	],
+
+	srConfig: ({ origin = 'bottom', delay = 200, viewFactor = 0.25 }) => ({
+		origin,
+		distance: '10px',
+		duration: 500,
+		delay,
+		rotate: { x: 0, y: 0, z: 0 },
+		opacity: 0,
+		scale: 1,
+		easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+		mobile: true,
+		reset: false,
+		useDelay: 'always',
+		viewFactor,
+		viewOffset: { top: 0, right: 0, bottom: 0, left: 0 }
+	})
 };
