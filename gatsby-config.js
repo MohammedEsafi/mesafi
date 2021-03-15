ad7818da60875b0a59944872f7aa1d651f9ef3dc
Mohammed Esafi <hello@mesafi.com>
2021-03-15 10:28:33 +0100

    disable (SSR) pages on full reloads during develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
 		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
 		author: `@gatsbyjs`
 	},
+	flags: {
+		DEV_SSR: false
+	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-image`,
