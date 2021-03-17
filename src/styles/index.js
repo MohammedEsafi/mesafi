d1b48c3bed08a3858aec76356c9f8d63c7e593b1
Mohammed Esafi <hello@mesafi.com>
2021-03-17 12:31:35 +0100

    make a grid component

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,5 @@
 export * as Theme from './theme';
 export { default as GlobalStyle } from './globalStyle';
 export { default as Flex } from './flex';
+export { default as Grid } from './grid';
 export { default as Button } from './button';
