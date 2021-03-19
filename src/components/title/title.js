22d540ee675b923a3bc353dc9559b2686fb60dd8
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-19 16:01:22 +0100

    style: scale section title

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { fontSize } from '@config';
+
+import { clamping } from '@utils';
+import { breakpoints, fontSize } from '@config';
 
 const Wrapper = styled.h1`
-	font-size: ${fontSize.xs}rem;
+	font-size: ${clamping(breakpoints.phone, breakpoints.desktop, fontSize.xxs, fontSize.xs)};
 	text-transform: uppercase;
 	font-weight: 500;
 	color: #aab2bd;
+	letter-spacing: 0.1em;
+	transform: scaleY(0.8);
 `;
 
 const Title = ({ children }) => <Wrapper>{children}</Wrapper>;
