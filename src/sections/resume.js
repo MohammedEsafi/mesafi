a527ff409e104880ac1ed6e04efd1b3c97a285e8
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-19 16:02:53 +0100

    style: update styles

--- src/sections/resume.js
+++ src/sections/resume.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import { Flex, Grid } from '@styles';
+import { Flex } from '@styles';
 import { clamping } from '@utils';
 import { breakpoints, padding, fontSize } from '@config';
 import { Title } from '@components';
@@ -9,82 +10,42 @@ import { Title } from '@components';
 const Wrapper = styled(Flex)`
 	max-width: var(--max-width);
 	margin: auto;
-	padding: 20px ${clamping(breakpoints.phone, breakpoints.desktop, padding.min, padding.max)};
-`;
+	padding: 50px ${clamping(breakpoints.phone, breakpoints.desktop, padding.min, padding.max)};
 
-const Content = styled(Grid)``;
-
-const Card = styled(Flex)`
-	& > h2 {
-		font-size: ${clamping(breakpoints.phone, breakpoints.desktop, fontSize.lg, fontSize.xl)};
-		font-weight: 500;
-	}
-
-	& > h3 {
-		font-size: ${clamping(breakpoints.phone, breakpoints.desktop, fontSize.md, fontSize.lg)};
-		font-weight: 600;
-	}
-
-	& > h4 {
+	& > div {
 		font-size: ${clamping(breakpoints.phone, breakpoints.desktop, fontSize.xs, fontSize.sm)};
-		font-weight: 500;
-		margin: 10px 0;
-	}
+		line-height: 2.5em;
 
-	& p {
-		color: ${({ theme }) => theme.text.subdued};
-		margin: 1.25em 0;
+		& > ul {
+			display: flex;
+			flex-wrap: wrap;
+			margin: 0;
+			padding: 0;
+			list-style: none;
+
+			& > li {
+				width: 50%;
+				color: ${({ theme }) => theme.text.subdued};
+			}
+		}
 	}
 `;
 
-const Resume = () => (
-	<Wrapper as='section' flexDirection='column'>
-		<Title>resume</Title>
-		<Content templateColumns='1fr 1fr' templateRows='auto auto'>
-			{/* <Card flexDirection='column'>
-				<h2>Work Experience</h2>
-				<>
-					<h3>Envato</h3>
-					<h4>FrontEnd Developer</h4>
-					<p>2018 - Current</p>
-				</>
-				<>
-					<h3>Freelancer</h3>
-					<h4>Product Designer</h4>
-					<p>2017 - 2018</p>
-				</>
-			</Card>
-			<Card flexDirection='column'>
-				<h2>Education</h2>
-				<>
-					<h3>Ibn zohr High School</h3>
-					<h4>bachelor of science physics.</h4>
-					<p>2014 - 2017</p>
-				</>
-				<>
-					<h3>1337 ― 42 Network</h3>
-					<h4>Software engineering school.</h4>
-					<p>2019 - Current</p>
-				</>
-			</Card> */}
-			<Card flexDirection='column'>
-				<h2>Skills</h2>
-				<p>Visual Development</p>
-				<p>Webflow Development</p>
-				<p>Front-End Development</p>
-				<p>HTML5/CSS3</p>
-				<p>No-Code Solutions</p>
-			</Card>
-			<Card flexDirection='column'>
-				<h2>Tools</h2>
-				<p>Figma</p>
-				<p>Notion</p>
-				<p>Illustrator</p>
-				<p>Sketch</p>
-				<p>Visual Studio Code</p>
-			</Card>
-		</Content>
-	</Wrapper>
-);
+const Resume = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			markdownRemark(fileAbsolutePath: { regex: "/resume/" }) {
+				html
+			}
+		}
+	`);
+
+	return (
+		<Wrapper as='section' flexDirection='column'>
+			<Title>resume</Title>
+			<div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+		</Wrapper>
+	);
+};
 
 export default Resume;
