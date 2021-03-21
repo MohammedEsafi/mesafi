2332e7b42850242e820f0489bfcbf09127135bee
Mohammed Esafi <hello@mesafi.com>
2021-03-21 15:57:13 +0100

    add `secondary` and `subdued` colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,8 @@
 export const lightTheme = {
 	background: '#FFFFFF',
 	onBackground: '#0B0C0D',
+	secondary: '#e6be42',
+	subdued: '#f4f6fb',
 	text: {
 		primary: '#0B0C0D',
 		subdued: 'red'
@@ -10,6 +12,8 @@ export const lightTheme = {
 export const darkTheme = {
 	background: '#0B0C0D',
 	onBackground: '#FFFFFF',
+	secondary: '#fcb97d',
+	subdued: '#aab2bd',
 	text: {
 		primary: '#FFFFFF',
 		subdued: '#656D78'
