759d0c871eb08d90bd7ee58487f741afc295aa3c
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-25 09:46:17 +0100

    chore: remove comments

--- content/1. Hero/index.md
+++ content/1. Hero/index.md
@@ -1,2 +1,1 @@
 I'm Mohammed Esafi — 👨‍💻 I pretend to know programming.
-<!---— Hi! I’m a software engineer, product designer, indie maker, and upcoming entrepreneur.-->
--- src/sections/contact.js
+++ src/sections/contact.js
@@ -2,49 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import { Flex, media } from '@styles';
-import { clamping, hex2rgba } from '@utils';
+import { Flex } from '@styles';
+import { clamping } from '@utils';
 import { breakpoints, padding, fontSize, email } from '@config';
 import Icons from '@components/icons';
 
 const Wrapper = styled(Flex)`
 	position: relative;
 	padding: 200px 0;
-
-	/* &:before {
-		content: '';
-		width: 60vw;
-		height: 60vw;
-		border-radius: 50%;
-		border: 1px solid ${({ theme }) => hex2rgba(theme.secondary, 0.05)};
-		position: absolute;
-		left: 0;
-		bottom: 100%;
-		transform: translate(-40%, 100%);
-
-		${media.phone`
-			width: 80vw;
-			height: 80vw;
-		`}
-	}
-
-	&:after {
-		content: '';
-		width: 50vw;
-		height: 50vw;
-		border-radius: 50%;
-		border: 1px solid ${({ theme }) => hex2rgba(theme.secondary, 0.05)};
-		position: absolute;
-		right: 0;
-		bottom: 50%;
-		transform: translate(20%, 50%);
-
-		${media.phone`
-			width: 70vw;
-			height: 70vw;
-			transform: translate(20%, 100%);
-		`}
-	} */
 `;
 
 const Content = styled(Flex)`
