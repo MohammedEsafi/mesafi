5802fa6c39f21c4b3cb184de50744e35654714e1
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-29 22:22:27 +0100

    feat: add mobile navigation menu

--- src/components/header/menu/menu.js
+++ src/components/header/menu/menu.js
@@ -1,37 +1,76 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import { Flex, media } from '@styles';
-import { navLinks } from '@config';
+import { navLinks, easing } from '@config';
 import { Title } from '@components/header/menu';
 
-const UL = styled(Flex)`
+const List = styled(Flex)`
 	list-style: none;
 	margin: 0;
 	padding: 0;
 
-	& > li > a {
-		color: ${({ theme }) => theme.text.primary};
-		padding: 20px 30px;
-	}
-
 	${media.tablet`
-		display: none;
+		position: absolute;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100vh;
+		left: 0;
+		top: -100vh;
+		background-color: ${({ theme }) => theme.secondary};
+		z-index: -1;
+		transform: translateY(${({ menuOpen }) => (menuOpen ? '100%' : '0')});
+		transition-property: transform;
+		transition-duration: 200ms;
+		transition-timing-function: ${easing};
 	`}
+
+	${({ length, menuOpen }) =>
+		[...new Array(length).keys()].reduce(
+			(accumulator, index) =>
+				accumulator.concat(`
+					& > li:nth-child(${index + 1}) {
+						transition-delay: ${menuOpen ? (index + 1) * 50 + 200 : 0}ms;
+					}
+				`),
+			''
+		)};
 `;
 
-const Menu = () => (
+const Item = styled(Flex)`
+	transition-property: visibility;
+	transition-duration: 200ms;
+	transition-timing-function: ${easing};
+
+	${media.tablet`
+		visibility: ${({ menuOpen }) => (menuOpen ? 'visible' : 'hidden')};
+	`};
+`;
+
+const Link = styled.a`
+	color: ${({ theme }) => theme.text.primary};
+	padding: 20px 30px;
+`;
+
+const Menu = ({ menuOpen }) => (
 	<Flex as='nav'>
-		<UL as='ul'>
+		<List as='ul' menuOpen={menuOpen} length={navLinks.length}>
 			{navLinks?.map(({ name, url }, index) => (
-				<Flex as='li' key={index}>
-					<a href={url}>
+				<Item as='li' key={index} menuOpen={menuOpen}>
+					<Link href={url}>
 						<Title text={name} />
-					</a>
-				</Flex>
+					</Link>
+				</Item>
 			))}
-		</UL>
+		</List>
 	</Flex>
 );
+
+Menu.propTypes = {
+	menuOpen: PropTypes.bool.isRequired
+};
 
 export default Menu;
