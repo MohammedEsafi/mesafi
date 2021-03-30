bfb57b62ca56d7e61232f00cf50dd43d02a4633c
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2021-03-30 12:08:05 +0100

    feat: add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { Layout } from '@components';
-// import SEO from '../components/seo';
+import { Flex } from '@styles';
+import { clamping } from '@utils';
+import { breakpoints } from '@config';
 
-const NotFoundPage = () => (
-	<Layout>
-		{/* <SEO title='404: Not found' /> */}
-		<h1>404: Not Found</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+const Content = styled(Flex)`
+	height: calc(100vh - var(--nav-height));
+
+	& > h1 {
+		font-size: ${clamping(breakpoints.phone, breakpoints.desktop, 9, 15)};
+		margin: 0;
+		line-height: normal;
+	}
+`;
+
+const NotFoundPage = ({ location }) => (
+	<Layout location={location} title='404' noFooter>
+		<Content flexDirection='column' alignItems='center' justifyContent='center'>
+			<h1>404</h1>
+			<p>Uh-Oh! The page not found!</p>
+		</Content>
 	</Layout>
 );
+
+NotFoundPage.propTypes = {
+	location: PropTypes.object.isRequired
+};
 
 export default NotFoundPage;
